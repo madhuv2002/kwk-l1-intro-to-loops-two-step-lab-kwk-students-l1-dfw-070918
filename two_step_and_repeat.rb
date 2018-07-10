fe1ac59ad7ce8fe6bad1a29a2d8ac7165c03360c
madhuv2002 <madhuv2002@gmail.com>
2018-07-10 23:41:25 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,51 +1,42 @@
-def first_steps
-  loop do
-    puts "Right Foot Back"
-    sleep(0.5)
-    puts "Left Foot Back"
-    sleep(0.5)
-    puts "Right Foot Back"
-    sleep(0.5)
-    puts "Stop"
-    sleep(1)
-  end 
-end
 
 def a_few_more_steps
-  loop do
-    puts "Right Foot Back"
-    sleep(0.5)
-    puts "Left Foot Back"
-    sleep(0.5)
-    puts "Right Foot Back"
-    sleep(0.5)
-    puts "Stop"
-    sleep(1)
-    puts "Right foot steps right and back"
-    sleep(0.5)
-    puts "Left foot crosses over right"
-    sleep(0.5)
-    puts "Right foot steps right"
-    sleep(0.5)
-    puts "Turn"
-    sleep(1)
-  end 
+ loop do
+   puts "Right foot back"
+   sleep(0.5)
+   puts "Left foot back"
+   sleep(0.5)
+   puts "Right foot back"
+   sleep(0.5)
+   puts "Stop"
+   sleep(1)
+   puts "Right foot steps right and back"
+   sleep(0.5)
+   puts "Left foot crosses over right"
+   sleep(0.5)
+   puts "Right foot steps right"
+   sleep(0.5)
+   puts "Turn"
+   sleep(1)
+ end
 end
 
-def how_many_steps
-  steps = 0
-  loop do
-    puts "steps"
-    steps += 1
-    if 
-      (steps % 2 == 0)
-      puts "Left"
-     else 
-        puts "Right"
-    end
-    sleep(0.5)
-  end
+def how_many_steps?
+ steps = 0
+ loop do
+   steps += 1
+   puts steps
+   if (steps % 2 == 0)
+   # if its an even number(divisible by 2) then puts left
+   # % keeps the remainder and divides it first
+   # == 0 is for, is an even number because there is no remainder
+     puts 'Left'
+   else
+     puts 'Right'
+   end
+   sleep(0.5)
+ end
 end
+
 
 def break_dance
  steps = 0
